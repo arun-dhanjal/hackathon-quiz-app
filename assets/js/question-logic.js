601e47e4ec5437baf6cc0b46d89de1f663e44c26
Amelia James <ameliafjjames@gmail.com>
2025-09-17 09:49:58 +0100

    Add questions object

---
+++ assets/js/question-logic.js
@@ -0,0 +1,30 @@
+let questions = [
+    {
+        question: "What is the capital of France?",
+        choices: ["Berlin", "Madrid", "Paris", "Rome"],
+        answer: 2
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        choices: ["Earth", "Mars", "Jupiter", "Saturn"],
+        answer: 1
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        choices: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+        answer: 3
+    },
+    {
+        question: "What is the percentage of the Earth covered by water?",
+        choices: ["51%", "61%", "71%", "81%"],
+        answer: 2
+    },
+    {
+        question: "What is the main ingredient of gnocchi?",
+        choices: ["Rice", "Potato", "Pasta", "Chocolate"],
+        answer: 1
+    }
+];
+
+currentQuestion = questions[0];
+currentQuestionNumber = 0;
