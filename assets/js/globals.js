7f5171f79676def909ae336fe98fd534ef6df8f9
vcarpentier <vjpcarpentier@gmail.com>
2025-09-18 17:19:08 +0100

    Fixed merge issues.

--- assets/js/globals.js
+++ assets/js/globals.js
@@ -2,6 +2,6 @@ let globals = {
     score: 0,
     buttonIDs: ["btnA", "btnB", "btnC", "btnD"],
     correctSound: new Audio("assets/audio/correct.mp3"),
-    wrongSound: new Audio("assets/audio/wrong.mp3")
+    wrongSound: new Audio("assets/audio/wrong.mp3"),
     currentQuestionIndex: 0
 }
